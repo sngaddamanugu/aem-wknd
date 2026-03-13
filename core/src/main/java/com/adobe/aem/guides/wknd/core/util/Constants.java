0bd39237306e1ca8b675a55c405d8efc281f1883
sackn <Sachin.KN@emerson.com>
2026-03-13 11:38:34 +0530

    tripinfo code changes

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/util/Constants.java
@@ -0,0 +1,25 @@
+package com.adobe.aem.guides.wknd.core.util;
+
+public final class Constants {
+
+    public static final String COUNTRY_SERVLET_RESOURCE_TYPE = "wknd/datasource/countries";
+    public static final String GET_METHOD = "GET";
+    public static final String TEXT = "text";
+    public static final String VALUE = "value";
+    public static final String FEATURED_IMAGE ="cq:featuredimage";
+    public static final String PROMO_BANNER_RESOURCE_TYPE = "wknd/components/promobanner";
+    public static final String TRIP_INFO_ACCORDION = "wknd/components/tripinfoaccordion";
+    public static final String DESTINATION_TYPE = "destinationType";
+
+    // Background
+    public static final String BACKGROUND_TYPE = "backgroundType";
+    public static final String BACKGROUND_COLOR = "backgroundColor";
+
+    // Image
+    public static final String FILE_REFERENCE = "fileReference";
+
+    // Buttons
+    public static final String PRIMARY_BUTTON = "primaryButton";
+    public static final String SECONDARY_BUTTON = "secondaryButton";
+
+}
