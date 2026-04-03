8a6d726d452539b0b7cee54a36321cbca839e38a
sackn <Sachin.KN@emerson.com>
2026-04-03 17:37:54 +0530

    workflow-custom-step

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/util/Constants.java
@@ -0,0 +1,15 @@
+package com.adobe.aem.guides.wknd.core.util;
+
+public class Constants {
+
+    private Constants() {
+        // Private constructor to hide the implicit public one.
+    }
+
+    //Workflows
+    public static final String DAM_BASE_PATH ="/content/dam";
+    public static final String WKND_BRAND_TAG ="brand:wknd-microsite";
+    public static final String METADATA_PATH = "/jcr:content/metadata";
+    public static final String CQ_TAGS = "cq:tags";
+
+}
